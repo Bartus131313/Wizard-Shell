5a0a348f20116db752e103f4d514a11388f77f4c
Bartus131313 <bartek.kansy@pisz.to>
2023-07-01 20:04:44 +0200

    Update BartAdd.py

--- addons/BartAdd.py
+++ addons/BartAdd.py
@@ -1,8 +1,12 @@
 from colorama import Fore, Style
+import subprocess
+import webbrowser
 
 def main(command, arg):
     if command == 'print':
         print_cmd(arg)
+    if command == 'start':
+        start_cmd(arg)
 
 # Commands
 
@@ -23,10 +27,17 @@ def print_cmd(arg):
         print(arg)
     print()
 
+def start_cmd(arg):
+    try:
+        subprocess.Popen(arg)
+        print()
+    except:
+        print(f'{Fore.RED}>>> Could not open "{arg}"{Style.RESET_ALL}\n')
+
 # Settings
 
 def commands():
-    return ['print']
+    return ['print', 'start']
 
 def commands_display():
-    return ['print [Color (#f00 / #0f0 / #00f)] [Message]']
+    return ['print [Color (#f00 / #0f0 / #00f)] [Message]', 'start [Argument]']
